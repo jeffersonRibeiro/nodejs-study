181dbcdd9192b36edfb59d02d209b8ac8fdb8c6d
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-08-30 19:08:11 -0300

    #

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 
 const username = os.userInfo().username;
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+const result = _.concat(arr1, arr2);
+
+console.log('result:', result);
+
 
 fs.appendFile('greetings.txt', `Hello ${username}`, err => {
     if(err) {
