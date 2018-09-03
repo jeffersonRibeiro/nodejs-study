435675e5eff6aea3aeb5e2257b384948f76427c6
Jefferson Ribeiro <jefferson.ribeiro.contato@gmail.com>
2018-09-02 23:51:35 -0300

    template partials

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const app = express();
 
+hbs.registerPartials(`${__dirname}/views/partials`);
 app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
 console.log(__dirname);
