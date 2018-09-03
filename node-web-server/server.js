b950527c0cd4691aebd9d8503a991bd023310db8
Jefferson Ribeiro <jefferson.ribeiro.contato@gmail.com>
2018-09-02 22:37:40 -0300

    static files folder

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,17 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(`${__dirname}/public`));
+
 app.get('/', (req, res) => {
-  res.send({
-    name: 'Jeff',
-    surname: 'Ribeiro',
-    skills: [
-      'NodeJs',
-      'ReactJs',
-      'Webpack',
-      'Mocha',
-    ],
-  });
+  res.send('Hello Express!');
 });
 
 app.listen(3000, 'localhost', () => {
